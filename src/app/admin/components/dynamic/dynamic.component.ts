edd611a0e2c42134783cbbd43582cc906d8a6dc8
icenine79 <vitor.ferreira@libretrend.com>
2019-12-03 02:50:23 +0000

    s

--- src/app/admin/components/dynamic/dynamic.component.ts
+++ src/app/admin/components/dynamic/dynamic.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { User } from '../../../shared/models/user';
-/* import { faFilm } from '@fortawesome/free-solid-svg-icons';
- */
+
 @Component({
   selector: 'app-dynamic',
   templateUrl: './dynamic.component.html',
